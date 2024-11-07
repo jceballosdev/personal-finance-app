ba3884109a983d916cd6cf84859801f8769c9f79
Juan Ceballos <jceballos.dev@outlook.com>
2024-11-07 00:12:34 -0500

    fix: corregir formato en archivos de configuración de TypeScript y Stylelint

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -1,9 +1,9 @@
 // stylelint.config.js
 module.exports = {
-  extends: ["stylelint-config-standard-scss"],
-  plugins: ["stylelint-scss"],
+  extends: ['stylelint-config-standard-scss'],
+  plugins: ['stylelint-scss'],
   rules: {
-    "at-rule-no-unknown": null,
-    "scss/at-rule-no-unknown": true,
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
   },
 };
